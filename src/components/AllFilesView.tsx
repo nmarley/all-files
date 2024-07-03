5c7d41341f22dfba9e48983d06b8506ed8cc216f
Nathan Marley <nathan.marley@gmail.com>
2024-07-02 20:40:15 -0500

    tidy

--- src/components/AllFilesView.tsx
+++ src/components/AllFilesView.tsx
@@ -26,9 +26,7 @@ const AllFilesView: React.FC<Props> = ({ supplierIds, user }) => {
 
     return (
         <Container>
-            <h3>
-            Total files: {count}
-            </h3>
+            <h3>Total files: {count}</h3>
             <Table
                 stickyHeader
                 striped
@@ -44,19 +42,15 @@ const AllFilesView: React.FC<Props> = ({ supplierIds, user }) => {
                         <Table.Th>Found At</Table.Th>
                     </Table.Tr>
                 </Table.Thead>
-            {files ? (
-                <Table.Tbody>
-                    {
-                        files.map(
-                            (file: FileData) => (
-                                <FileRow key={file.id} file={file} />
-                            )
-                        )
-                    }
-                </Table.Tbody>
-            ) : (
-                <p>No data available.</p>
-            )}
+                {files ? (
+                    <Table.Tbody>
+                        {files.map((file: FileData) => (
+                            <FileRow key={file.id} file={file} />
+                        ))}
+                    </Table.Tbody>
+                ) : (
+                    <p>No data available.</p>
+                )}
             </Table>
         </Container>
     );
